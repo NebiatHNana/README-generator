af564bc25dc4623b6d20eb993589b5b5b48b7842
Nebiat  Nana <nebiatnana@Nebiats-MacBook-Air.local>
2022-11-12 15:09:59 -0500

    README Generator with full funcitonaltiy

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# README-GENERATOR
 
+  # README-GENERATOR
+  ![License](https://img.shields.io/badge/license-MIT-green.svg)
+  # Table of contents
+  1. [Description](#description)
+  2. [Installation](#installation)
+  3. [Usage](#usage)
+  4. [License](#license)
+  5. [Contributing](#contributing)
+  6. [Questions](#questions)
+  ## Description
+  Generates a README.md file for your porject
+  ## Installation
+  Pulll or Clone from my gitHub
+  ## Usage
+  Run node index.js in the terminal and fill out each questions
+  ## License
+  MIT
+  this project is licensed under the MIT license
+  ## Contributing
+  Nebiat Hailemichael
+  ## Questions
+  if you have any additional questions, you can reach me at nebiathn@gmail.com
+  [github](https://github.com/NebiatHNana)
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -9,11 +9,36 @@ function renderLicenseLink(license) {}
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
+function renderLicenseBadge(license) {
+return `![License](https://img.shields.io/badge/license-${license}-green.svg)`
+}
 
-// TODO: Create a function to generate markdown for README
+// function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  return `
+  # ${data.title}
+  ${renderLicenseBadge(data.license)}
+  # Table of contents
+  1. [Description](#description)
+  2. [Installation](#installation)
+  3. [Usage](#usage)
+  4. [License](#license)
+  5. [Contributing](#contributing)
+  6. [Questions](#questions)
+  ## Description
+  ${data.description}
+  ## Installation
+  ${data.install}
+  ## Usage
+  ${data.usage}
+  ## License
+  ${data.license}
+  this project is licensed under the ${data.license} license
+  ## Contributing
+  ${data.contributing}
+  ## Questions
+  if you have any additional questions, you can reach me at ${data.email}
+  [github](https://github.com/${data.github})
 `;
 }
 
